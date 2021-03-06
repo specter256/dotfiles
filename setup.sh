80cfd05f4bd2d8e4c0a313f30dfc84edab8d62b4
Alexander <int3rrup7@gmail.com>
2021-03-06 21:15:26 +0300

    packages, arc-theme

--- setup.sh
+++ setup.sh
@@ -99,12 +99,7 @@ install_numix_icon() {
 }
 
 install_arc_theme() {
-  sudo apt-get install -y autoconf automake pkg-config libgtk-3-dev git &&
-  git clone https://github.com/horst3180/arc-theme --depth 1 &&
-  cd arc-theme &&
-  ./autogen.sh --prefix=/usr &&
-  sudo make install &&
-  cd ..
+  sudo apt install arc-theme
 }
 
 run_numix_script() {
